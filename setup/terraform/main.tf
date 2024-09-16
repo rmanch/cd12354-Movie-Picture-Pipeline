e249a3e6d47438f4d1bde9b288d038e4c8ff662e
rmanch <rahul.manchanda@gmail.com>
2024-09-16 12:47:34 +0530

    New files

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -252,72 +252,15 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-######################
-# CodeBuild Resources
-######################
-# Create a CodeBuild project
-resource "aws_codebuild_project" "codebuild" {
-  name          = "udacity"
-  description   = "Udacity CodeBuild project"
-  service_role  = aws_iam_role.codebuild.arn
-  build_timeout = 60
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:5.0"
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "CODEBUILD"
-    privileged_mode             = true
-  }
-
-  source {
-    type            = "GITHUB"
-    location        = "https://github.com/rmanch/cd12354-Movie-Picture-Pipeline/"
-    git_clone_depth = 1
-    buildspec       = "buildspec.yml"
-  }
-
-  cache {
-    type = "NO_CACHE"
-  }
-}
-
-# Create the Codebuild Role
-resource "aws_iam_role" "codebuild" {
-  name = "codebuild-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "codebuild.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-# Attach the IAM policy to the codebuild role
-resource "aws_iam_role_policy_attachment" "codebuild" {
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
-  role       = aws_iam_role.codebuild.name
-}
-
 ####################
 # Github Action role
 ####################
-#resource "aws_iam_user" "github_action_user" {
- # name = "github-action-user"
-#}
+resource "aws_iam_user" "github_action_user" {
+ name = "github-action-user1"
+}
 
 resource "aws_iam_user_policy" "github_action_user_permission" {
-  user   = "github-action-user" # aws_iam_user.github_action_user.name
+  user   =   aws_iam_user.github_action_user.name
   policy = data.aws_iam_policy_document.github_policy.json
 }
 
