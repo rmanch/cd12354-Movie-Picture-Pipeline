804c3a208d27404ecbccdd286a4a05aa5f690ec4
rmanch <rahul.manchanda@gmail.com>
2024-09-16 15:11:57 +0530

    new files

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -256,18 +256,18 @@ data "aws_iam_policy_document" "assume_role_policy" {
 # Github Action role
 ####################
 resource "aws_iam_user" "github_action_user" {
- name = "github-action-user2"
+ name = "github-action-user1"
 }
 
-resource "aws_iam_user_policy" "github_action_user_permission" {
-  user   =   aws_iam_user.github_action_user.name
-  policy = data.aws_iam_policy_document.github_policy.json
-}
+#resource "aws_iam_user_policy" "github_action_user_permission" {
+#  user   =   aws_iam_user.github_action_user.name
+#  policy = data.aws_iam_policy_document.github_policy.json
+#}
 
-data "aws_iam_policy_document" "github_policy" {
-  statement {
-    effect    = "Allow"
-    actions   = ["ecr:*", "eks:*", "ec2:*"] # "iam:GetUser"
-    resources = ["*"]
-  }
+#data "aws_iam_policy_document" "github_policy" {
+#  statement {
+#    effect    = "Allow"
+#    actions   = ["ecr:*", "eks:*", "ec2:*"] # "iam:GetUser"
+#    resources = ["*"]
+#  }
 }
